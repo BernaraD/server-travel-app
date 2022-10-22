25e408be3edc55921c3fe26f56ca401ee8b69a86
bernarad <bernara.dzhaparova@gmail.com>
2022-10-22 05:24:42 -0400

    Image handler, validation

--- db/mongo.js
+++ db/mongo.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 function connnectToMongoDB() {
-    let MONGO_DB = "mongodb://localhost:27017/noble-mongodb-intro";
+    // let MONGO_DB = "mongodb://localhost:27017/noble-mongodb-intro";
 
     mongoose
-        .connect(MONGO_DB)
+        .connect('mongodb+srv://Bernara:Summer2022@cluster0.b9a6zfb.mongodb.net/test')
         .then(() => {
             console.log("mongodb+srv://Bernara:Summer2022@cluster0.b9a6zfb.mongodb.net/test");
         })
